b75c8f41a130ed75a973eb06abd1515c5f185ef8
AlexeyTuhtiaev <alexey.tuhtiaev@gmail.com>
2019-04-11 21:03:00 +0300

    Base models and fake repository

---
+++ TripShare.Back/Controllers/TripController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TripShare.Back.Models;
+
+namespace TripShare.Back.Controllers
+{
+    [Route("api/[controller]")]
+    public class TripController : Controller
+    {
+        private Repository _repository;
+
+        public TripController(Repository repo)
+        {
+            _repository = repo;
+        }
+       
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<Trip> Get()
+        {
+            return _repository.Get();
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public Trip Get(int id)
+        {
+           return _repository.Get(id);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]Trip newTriptrip)
+        {
+            _repository.Add(newTriptrip);
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]Trip tripToUpdate)
+        {
+            _repository.Update(tripToUpdate);
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            _repository.Delete(id);
+        }
+    }
+}
---
+++ TripShare.Back/Models/Repository.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TripShare.Back.Models
+{
+    public class Repository
+    {
+        private List<Trip> MyTrips = new List<Trip>
+        {
+            new Trip
+            {
+                Id = 1,
+                Name = ".NET Summit",
+                StartDate = new DateTime(2019, 5, 25),
+                EndDate = new DateTime(2019, 5, 28)
+            },
+            new Trip
+            {
+                Id = 2,
+                Name = "Relaxation Trip",
+                StartDate = new DateTime(2019, 6, 15),
+                EndDate = new DateTime(2019, 6, 28)
+            },
+            new Trip
+            {
+                Id = 3,
+                Name = "Student Trip",
+                StartDate = new DateTime(2019, 8, 05),
+                EndDate = new DateTime(2019, 10, 29)
+            }
+        };
+
+        public List<Trip> Get()
+        {
+            return MyTrips;
+        }
+
+        public Trip Get(int id)
+        {
+            return MyTrips.First(t => t.Id == id);
+        }
+
+        public void Add(Trip newTrip)
+        {
+            MyTrips.Add(newTrip);
+        }
+
+        public void Update(Trip tripToUpdate)
+        {
+            MyTrips.Remove(MyTrips.First(t => t.Id == tripToUpdate.Id));
+            MyTrips.Add(tripToUpdate);
+        }
+
+        public void Delete(int id)
+        {
+            MyTrips.Remove(MyTrips.First(t => t.Id == id));
+        }
+    }
+}
