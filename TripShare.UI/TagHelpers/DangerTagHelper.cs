86345914a79159d07f786f30fae4098778d91c11
AlexeyTuhtiaev <alexey.tuhtiaev@gmail.com>
2019-04-24 09:04:50 +0300

    Work with TagHelper

---
+++ TripShare.UI/TagHelpers/DangerTagHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.Runtime.TagHelpers;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace TripShare.UI.TagHelpers
+{
+    // You may need to install the Microsoft.AspNetCore.Razor.Runtime package into your project
+    [HtmlTargetElement(Attributes = "danger")]
+    public class DangerTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (context.AllAttributes.ContainsName("class"))
+            {
+                output.Attributes.SetAttribute("class", string.Concat(output.Attributes["class"].Value, "text-danger"));
+            }
+            else
+            {
+                output.Attributes.SetAttribute("class", "text-danger");
+            }
+        }
+    }
+}
